71ebb783c6877faed01d8d4319cbf79a6ac3b96f
chadthom7 <chadthom7@gmail.com>
2019-12-05 21:13:11 -0500

    starting point

---
+++ dvrouter.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class dvrouter {
+
+public dvrouter()//String address, int port) 
+{ 
+
+	// System.out.println("Connected"); // Connected
+	
+// ------------------- Receive Data ----------------------
+// ------------------- Send Data -------------------------
+/*
+	ReadingThread r = new ReadingThread();
+	SendingThread s = new SendingThread();
+	new Thread(r).start();
+	new Thread(s).start();
+
+*/
+}
+
+public static void main(String args[]) 
+{ 
+	if (args.length != 3) {
+		System.out.println("");
+	}
+	else {
+		//chat_client client = new chat_client(args[0], Integer.parseInt(args[1])); 
+	}
+} 
+}
